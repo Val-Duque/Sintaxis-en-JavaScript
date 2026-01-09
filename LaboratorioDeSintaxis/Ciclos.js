1c8a2343d073629c60d3dfadcf1a1d7c73476600
Violelo09 <valentinaduque034@gmail.com>
2026-01-09 09:38:54 -0500

    organizar de ciclo

---
+++ LaboratorioDeSintaxis/Ciclos.js
@@ -0,0 +1,11 @@
+let passwordCorrecto = "1234";
+let intentos = 0;
+
+// 
+while (intentos < 3) {
+    let passwordIngresada = prompt("Ingresa tu contraseña: ");
+}
+
+if (passwordIngresada === passwordCorrecto) {
+    console.log("Acesso concedido");
+}
